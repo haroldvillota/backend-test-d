c331de34b22d54a82a249ef1af6163ff17fa1a22
Harold Villota <aroldf@gmail.com>
2023-05-07 08:22:58 -0500

    improve project structure - move models to folder model - fixed seedDb.js script to find models in the new folder - fixed app.js to find models in the new folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
+const {sequelize} = require('./models')
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
+
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
+
+
 
 /**
  * FIX ME!
@@ -18,4 +21,5 @@ app.get('/contracts/:id',getProfile ,async (req, res) =>{
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
+
 module.exports = app;
